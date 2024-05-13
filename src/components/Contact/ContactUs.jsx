a44e2bc34a6a58a46a9d200fb9dff4ec5877428b
dhavalnerds97 <dhaval.prajapati@nerdshouse.com>
2024-05-13 18:21:42 +0530

    Update ContactUs.jsx

--- src/components/Contact/ContactUs.jsx
+++ src/components/Contact/ContactUs.jsx
@@ -46,24 +46,28 @@ const ContactUs = () => {
     }
   };
   return (
-    <div className="w-full text-wrap">
+    <div className="w-full text-wrap overflow-y-auto min-h-fit">
       <div className="flex h-auto p-2 py-4 ">
         <div className="bg-amber-500 w-1"></div>
         <div className="pl-[20px] text-gray-100">
-          <p className="text-4xl pt-2 font-urbanist pb-4">
+          <p className="text-4xl font-urbanist pb-2">
             Wish to take a sneak peek into our success stories?
           </p>
-          <p className="font-urbanist pb-2">
+          <p className="font-urbanist">
             Narrow down your search and hop on to your preferred industry.
           </p>
         </div>
       </div>
-      <form className="lg:max-w-sm " onSubmit={handleSubmit}>
-        <div className="flex flex-col mb-3 p-2">
-          <label
-            htmlFor="name"
-            className="ps-1 font-nunito  text-gray-100 text-lg "
-          >
+      {/* <form onSubmit={handleSubmit}> */}
+      <form
+        className="lg:max-w-sm "
+        action="https://formspree.io/f/xayraepk"
+        encType="multipart/form-data"
+        // onSubmit={handleSubmit}
+        method="POST"
+      >
+        <div className="flex flex-col mb-1 p-2">
+          <label htmlFor="name" className="ps-1 font-nunito  text-gray-100 ">
             Name
           </label>
           <input
@@ -71,45 +75,56 @@ const ContactUs = () => {
             id="name"
             type="text"
             ref={nameRef}
+            name="name"
             placeholder="Enter your name"
-            className=" bg-neutral-900 font-slab focus:outline-none w-full h-6 px-3 py-5 text-lg text-gray-100 border-slate-400 focus:border-amber-500 ease-in-out transition-all duration-700"
+            className=" bg-neutral-900 font-slab focus:outline-none w-full h-6 px-3 py-5 text-gray-100 border-slate-400 focus:border-amber-500 ease-in-out transition-all duration-700"
           />
         </div>
-        <div className="flex flex-col mb-3 p-2">
-          <label
-            htmlFor="email"
-            className="ps-1 font-nunito  text-gray-100 text-lg "
-          >
+        <div className="flex flex-col mb-1 p-2">
+          <label htmlFor="email" className="ps-1 font-nunito  text-gray-100 ">
             Email
           </label>
           <input
             required
             id="email"
             type="email"
+            name="email"
             ref={emailRef}
             placeholder="Enter your email"
-            className=" bg-neutral-900 font-slab focus:outline-none w-full h-6 px-3 py-5 text-lg text-gray-100 border-slate-400 focus:border-amber-500 ease-in-out transition-all duration-700"
+            className=" bg-neutral-900 font-slab focus:outline-none w-full h-6 px-3 py-5 text-gray-100 border-slate-400 focus:border-amber-500 ease-in-out transition-all duration-700"
           />
         </div>
-        <div className="flex flex-col mb-3 p-2">
-          <label
-            htmlFor="contact"
-            className="ps-1 font-nunito text-gray-100 text-lg"
-          >
+        <div className="flex flex-col mb-1 p-2">
+          <label htmlFor="contact" className="ps-1 font-nunito text-gray-100">
             Contact Number
           </label>
           <input
             required
             id="contact"
             type="tel"
+            name="contact"
             ref={contactRef}
             placeholder="Enter your contact number"
-            className=" bg-neutral-900 font-slab focus:outline-none w-full h-6 px-3 py-5 text-lg text-gray-100 border-slate-400 focus:border-amber-500 ease-in-out transition-all duration-700"
+            className=" bg-neutral-900 font-slab focus:outline-none w-full h-6 px-3 py-5 text-gray-100 border-slate-400 focus:border-amber-500 ease-in-out transition-all duration-700"
             pattern="\d{8,}"
             title="Contact number must be at least 8 digits long"
           />
         </div>
-        <div className="flex flex-col mt-3 mb-3 p-2">
+        <div className="flex flex-col mb-1 p-2">
+          <label htmlFor="message" className="ps-1 font-nunito  text-gray-100 ">
+            Message
+          </label>
+          <input
+            required
+            id="message"
+            type="textarea"
+            ref={nameRef}
+            name="message"
+            placeholder="Enter your message"
+            className=" bg-neutral-900 font-slab focus:outline-none w-full h-6 px-3 py-5 text-gray-100 border-slate-400 focus:border-amber-500 ease-in-out transition-all duration-700"
+          />
+        </div>
+        <div className="flex flex-col mt-3 mb-1 p-2">
           <button className="px-12 py-3 border-2 border-white bg-neutral-900 text-neutral-100 font-bold font-nunito hover:shadow-[4px_4px_0px_0px_rgba(255,215,0)] transition duration-300">
             Submit
           </button>

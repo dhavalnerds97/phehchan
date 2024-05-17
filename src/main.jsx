e64829ef6ac60fbcb781341a623d346d11748fcb
dhavalnerds97 <dhaval.prajapati@nerdshouse.com>
2024-05-17 17:07:43 +0530

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import appRouter from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "./utils/ThemeContext.jsx";
 import ActiveSectionContextProvider from "./utils/active-section-context.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
       <ActiveSectionContextProvider>
         <RouterProvider router={appRouter} />
+        <Analytics />
       </ActiveSectionContextProvider>
     </ThemeProvider>
   </React.StrictMode>
